b087063aa9cf718ee7442c43a202cf9060041a76
Dario Milani <dmilani855@gmail.com>
2023-04-25 22:13:27 +0200

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dario Milani on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
